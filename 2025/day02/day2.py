c346dc54ef5f62060e92e3b43b3c077b75d32f8d
erikm98 <erikmeijer98@gmail.com>
2025-12-02 14:20:03 +0100

    Replaced textwrap with list comprehension

--- 2025/day02/day2.py
+++ 2025/day02/day2.py
@@ -1,6 +1,4 @@
 import argparse
-import textwrap
-from time import time
 
 def load_input(file_path:str) -> list[str]:
     read_file = open(file_path, "r") 
@@ -11,19 +9,19 @@ def load_input(file_path:str) -> list[str]:
         data+=[str(i) for i in range(int(x),int(y)+1)]
     return data
 
-def sum_invalid(data:list(list[int])) -> int:
+def sum_invalid(data:list[list[int]]) -> int:
     invalid_sum = 0
     for i in data:
         if i[:int(len(i)/2)] == i[int(len(i)/2):]:
             invalid_sum += int(i)
     return invalid_sum
 
-def strict_sum_invalid(data:list(list[int])) -> int:
+def strict_sum_invalid(data:list[list[int]]) -> int:
     invalid_sum = 0
     for i in data:
         for j in range(1,int(len(i)/2)+1):
             if len(i)%j == 0:
-                ls = textwrap.wrap(i, j)
+                ls = [i[k:k+j] for k in range(0, len(i), j)]
                 if [ls[0]]*len(ls) == ls:
                     invalid_sum += int(i)
                     break
@@ -37,10 +35,9 @@ def main():
     data = load_input(args.input)
     part1_result = sum_invalid(data)
     part2_result = strict_sum_invalid(data)
-    
+
     print(f"Part 1 - Sum of invalid entries: {part1_result}")
     print(f"Part 2 - Sum of strictly invalid entries: {part2_result}")
 
 if __name__ == "__main__":
     main()
-
